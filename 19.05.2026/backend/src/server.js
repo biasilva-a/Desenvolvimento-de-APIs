e1d5cf30e464b25a9d3471162c08fe9ba63fc41f
Bia <bianca_og_silva@estudante.sesisenai.org.br>
2026-05-19 15:54:22 -0300

    Configuração backend

--- 19.05.2026/backend/src/server.js
+++ 19.05.2026/backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
+import 'dotenv/config';
 
-import { filmesRoute } from './routes/filmes.routes.js';
-import { filmesService } from './service/filmes.service.js';
+import { filmesRoute } from './routes/filmes.route.js';
 
 const app = express();
 const PORT = process.env.API_PORT || 3000;
@@ -9,11 +9,11 @@ const PORT = process.env.API_PORT || 3000;
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    const filmes = await filmesService.getALL();
-    return res.json(filmes);
+    res.json("Bem-vindo á API de Filmes!");
 });
 
 app.use('/filmes', filmesRoute);
+
 app.listen(PORT, () => {
     console.log(`API rodando em http://localhost:${PORT}`);
 });
